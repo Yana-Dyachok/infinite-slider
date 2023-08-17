afa08bc370f811142da68df00baf53ba4add2367
Yana-Dyachok <annetart1994@gmail.com>
2023-08-17 16:03:49 +0300

    fix: fix mistakes in btn  arrow-btn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const slider = document.querySelector('.slider'),
     sliderItems = document.querySelectorAll('.slider-item'),
-    prevButton = document.querySelector('.prev-button'),
-    nextButton = document.querySelector('.next-button'),
+    prevButton = document.querySelector('.prev-btn'),
+    nextButton = document.querySelector('.next-btn'),
     firstCardWidth = sliderItems[0].offsetWidth;
 console.log(firstCardWidth);
 
 function clickButton(isNext) {
-   slider.scrollLeft+=isNext?-firstCardWidth:firstCardWidth;
+   slider.scrollLeft+=isNext?firstCardWidth:-firstCardWidth;
 }
 
 nextButton.addEventListener('click', () => clickButton(true));
 prevButton.addEventListener('click', () => clickButton(false));
+
