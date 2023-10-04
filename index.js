1259b7cf2d682eb36f3f5a3e136e4c6b8aa67baa
Yana-Dyachok <annetart1994@gmail.com>
2023-10-05 00:53:31 +0300

    fix: fix code and make infinite slider

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-const slider = document.querySelector('.slider'),
-    sliderItems = document.querySelectorAll('.slider-item'),
-    prevButton = document.querySelector('.prev-btn'),
-    nextButton = document.querySelector('.next-btn'),
-    firstCardWidth = (sliderItems[0].offsetWidth)+15;
-console.log(firstCardWidth);
+document.addEventListener("DOMContentLoaded", function(event) {
+    const slider = document.querySelector(".slider");
+    const sliderItems = document.querySelectorAll(".slider-item");
+    const prevBtn = document.querySelector(".prev-btn");
+    const nextBtn = document.querySelector(".next-btn");
+    const slideWidth = sliderItems[0].clientWidth;
+    let currentIndex = 0;
+    let amountImg=Math.ceil(slider.clientWidth/slideWidth)
+    
+    prevBtn.addEventListener("click", () => {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex =sliderItems.length-amountImg;
+            slider.style.transition = "none";
+            slider.style.transform = `translateX(${-slideWidth * (sliderItems.length)}px)`;
+            setTimeout(() => {
+                slider.style.transition = "transform 1s ease-in-out";
+                currentIndex = sliderItems.length-amountImg;
+                updateSlider();
+            }, 0);
+        }        
+        else {
+            updateSlider();
+        }
+    });
 
-function clickButton(isNext) {
-   slider.scrollLeft+=isNext?firstCardWidth:-firstCardWidth;
-}
+    nextBtn.addEventListener("click", () => {
+        currentIndex++;
+        if (currentIndex >= sliderItems.length-(amountImg-1)) {
+            currentIndex =amountImg===1?0:sliderItems.length-amountImg;
+            slider.style.transition = "none";
+            slider.style.transform = `translateX(${slideWidth}px)`;
+            setTimeout(() => {
+                slider.style.transition = "transform 1s ease-in-out";
+                currentIndex = 0;
+                updateSlider();
+            }, 0);
+        } else {
+            updateSlider();
+        }
+    });
 
-nextButton.addEventListener('click', () => clickButton(true));
-prevButton.addEventListener('click', () => clickButton(false));
-
+    function updateSlider() {
+        const translateX = -currentIndex * slideWidth;
+        slider.style.transform = `translateX(${translateX}px)`;
+    }
+});
